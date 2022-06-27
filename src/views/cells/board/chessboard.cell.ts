c83aa10d7268e4465b41439b63a2173e7d9df306
agentx-cgn <agentx.cgn@gmail.com>
2022-06-28 00:04:06 +0200

    moves pieces

--- src/views/cells/board/chessboard.cell.ts
+++ src/views/cells/board/chessboard.cell.ts
@@ -1,6 +1,6 @@
 
 import m from 'mithril';
-import { Chessboard, BORDER_TYPE, COLOR, MARKER_TYPE } from "../../../extern/cm-chessboard/index";
+import { Chessboard, BORDER_TYPE, COLOR, MARKER_TYPE, INPUT_EVENT_TYPE } from "../../../extern/cm-chessboard/index";
 import './chessboard.cell.scss';
 
 import { ICellComponent } from '@app/domain';
@@ -18,28 +18,45 @@ const ChessboardCell: ICellComponent = {
 
   oncreate (  ) {
 
-      // const { board } = vnode.attrs;
+    // const { board } = vnode.attrs;
 
-      chessBoard = new Chessboard( $('div.chessboard'), {
-          position: "empty", // set as fen, "start" or "empty"
-          orientation: COLOR.white, // white on bottom
-          responsive: true, // resizes the board based on element size
-          animationDuration: 300, // pieces animation duration in milliseconds. Disable all animation with `0`.
-          style: {
-              cssClass: "default", // set the css theme of the board, try "green", "blue" or "chess-club"
-              showCoordinates: true, // show ranks and files
-              borderType: BORDER_TYPE.none, // "thin" thin border, "frame" wide border with coordinates in it, "none" no border
-              aspectRatio: 1, // height/width of the board
-              moveFromMarker: MARKER_TYPE.frame, // the marker used to mark the start square
-              moveToMarker: MARKER_TYPE.frame, // the marker used to mark the square where the figure is moving to
-          },
-          sprite: {
-              url: "static/chessboard/chessboard-sprite.svg", // pieces and markers are stored in a sprite file
-              size: 40, // the sprite tiles size, defaults to 40x40px
-              cache: true // cache the sprite
-          },
-        }
-      );
+    chessBoard = new Chessboard( $('div.chessboard'), {
+      position: "start", // set as fen, "start" or "empty"
+      orientation: COLOR.white, // white on bottom
+      responsive: true, // resizes the board based on element size
+      animationDuration: 300, // pieces animation duration in milliseconds. Disable all animation with `0`.
+      style: {
+        cssClass: "green", // set the css theme of the board, try "green", "blue" or "chess-club"
+        showCoordinates: true, // show ranks and files
+        borderType: BORDER_TYPE.none, // "thin" thin border, "frame" wide border with coordinates in it, "none" no border
+        aspectRatio: 1, // height/width of the board
+        moveFromMarker: MARKER_TYPE.frame, // the marker used to mark the start square
+        moveToMarker: MARKER_TYPE.frame, // the marker used to mark the square where the figure is moving to
+      },
+      sprite: {
+        url: "static/chessboard/chessboard-sprite.svg", // pieces and markers are stored in a sprite file
+        size: 40, // the sprite tiles size, defaults to 40x40px
+        cache: true // cache the sprite
+      },
+    });
+
+    chessBoard.enableMoveInput((event: any) => {
+      switch (event.type) {
+          case INPUT_EVENT_TYPE.moveStart:
+              console.log(`moveStart: ${event.square}`)
+              // return `true`, if input is accepted/valid, `false` aborts the interaction, the piece will not move
+              return true;
+          case INPUT_EVENT_TYPE.moveDone:
+              console.log(`moveDone: ${event.squareFrom}-${event.squareTo}`)
+              // return true, if input is accepted/valid, `false` takes the move back
+              return true;
+          case INPUT_EVENT_TYPE.moveCanceled:
+              console.log(`moveCanceled`);
+              return false; //??
+          default:
+            return false;
+      }
+      }, COLOR.white);
 
       console.log(chessBoard);
 
